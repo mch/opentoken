c42dd85a5c81611637927c4919a114a882bf276e
Matt Hughes <matt.c.hughes@gmail.com>
2012-12-12 23:35:08 -0700

    Added gloss for handling byte serialization. Can hash, encrypt, and decrypt.

--- src/mch/opentoken.clj
+++ src/mch/opentoken.clj
@@ -1,7 +1,102 @@
-(ns mch.opentoken)
+(ns mch.opentoken
+  (:require [gloss core io]))
 
-(defn encode [payload & {:keys [cipher iv] :or {cipher :aes-256 iv nil}}]
+(def cipher-suites {:none 0 :aes-256 1 :aes-128 2 :3des-168 3})
+
+(def opentoken-frame {:otk (gloss.core/string :utf-8 :length 3)
+                      :version :byte
+                      :cipher-suite :byte
+                      :hmac (gloss.core/finite-block 20)
+                      :iv (gloss.core/finite-block :byte)
+                      :key-info (gloss.core/finite-block :byte)
+                      :payload (gloss.core/finite-block :int16)})
+
+(gloss.core/defcodec opentoken opentoken-frame)
+
+(def payload-len-frame {:payload-len :int16})
+  
+(gloss.core/defcodec payload-len-codec payload-len-frame)
+
+(defn stringify-payload [payload]
+  "Converts a map to a OpenToken payload string."
+  {:pre [(map? payload)]}
+  (reduce #(format "%s=%s\r\n%s" (first %2) (second %2) %1) "" payload))
+
+;; updating should be equivalent to creating a single byte-array and doing it all at once. 
+(defn init-hmac [version suite iv key-info payload]
+  (let [digester (java.security.MessageDigest/getInstance "SHA-1")]
+    (.update digester (byte version))
+    (.update digester (byte suite))
+    (if-not (nil? iv)
+      (if (= (type iv) (type (byte-array 0)))
+        (.update digester iv)))
+    (if-not (nil? key-info)
+      (if (= (type key-info) (type (byte-array 0)))
+        (.update digester key-info)))
+    (.update digester (.array (gloss.io/contiguous (gloss.io/encode payload-len-codec {:payload-len (count payload)}))))
+    digester))
+
+(defn update-hmac-with-clear-payload [digester payload]
+  (.update digester (.getBytes payload "utf-8")))
+
+;; Use java.util.zip.DeflaterOutputSteam
+(defn deflate [payload]
   nil)
 
-(defn decode [payload & {:keys [cipher iv] :or {cipher :aes-256 iv nil}}]
+;; http://stackoverflow.com/questions/992019/java-256-bit-aes-password-based-encryption
+(defn encrypt-aes [secret salt payload & {:keys [key-length] :or {key-length 256}}]
+  (if-not (string? secret) (throw (.IllegalArgumentException "Secret must be a string.")))
+  (if-not (string? salt) (throw (.IllegalArgumentException "Salt must be a string.")))
+  (let [key-factory (javax.crypto.SecretKeyFactory/getInstance "PBKDF2WithHmacSHA1")
+        key-spec (javax.crypto.spec.PBEKeySpec. (.toCharArray secret) (.getBytes salt) 65536 key-length)
+        key (javax.crypto.spec.SecretKeySpec. (.getEncoded (.generateSecret key-factory key-spec)) "AES")
+        cipher (javax.crypto.Cipher/getInstance "AES/CBC/PKCS5Padding")
+        _ (.init cipher javax.crypto.Cipher/ENCRYPT_MODE key)
+        params (.getParameters cipher)
+        iv (.getIV (.getParameterSpec params javax.crypto.spec.IvParameterSpec))
+        ciphertext (.doFinal cipher (.getBytes payload "UTF-8"))]
+    {:iv iv :ciphertext ciphertext}))
+
+(defn decrypt-aes [secret salt iv ciphertext & {:keys [key-length] :or {key-length 256}}]
+  (if-not (string? secret) (throw (.IllegalArgumentException "Secret must be a string.")))
+  (if-not (string? salt) (throw (.IllegalArgumentException "Salt must be a string.")))
+  (let [key-factory (javax.crypto.SecretKeyFactory/getInstance "PBKDF2WithHmacSHA1")
+        key-spec (javax.crypto.spec.PBEKeySpec. (.toCharArray secret) (.getBytes salt) 65536 key-length)
+        key (javax.crypto.spec.SecretKeySpec. (.getEncoded (.generateSecret key-factory key-spec)) "AES")
+        cipher (javax.crypto.Cipher/getInstance "AES/CBC/PKCS5Padding")
+        _ (.init cipher javax.crypto.Cipher/DECRYPT_MODE key (IvParameterSpec. iv))
+        plaintext (String. (.doFinal cipher ciphertext) "UTF-8")]
+    plaintext))
+
+
+        
+(defn encrypt [cipher payload]
+  nil)
+
+(defn create-frame [payload]
+  nil)
+
+(defn base64encode [data]
+  nil)
+
+(defn replace-padding [base64data]
+  nil)
+
+(defn encode [payload secret & {:keys [cipher salt] :or {cipher :none salt nil}}]
+  "Returns a string representing the encrypted OpenToken."
+  (if-not (map? payload)
+    (throw (java.lang.IllegalArgumentException. "Payload must be a map.")))
+  (if-not (contains? cipher-suites cipher)
+    (throw (java.lang.IllegalArgumentException. "Cipher must be one of :none, :aes-256, :aes-128, or :3des-168.")))
+  nil)
+;  (->> payload
+;       (stringify-payload)
+;       (init-hmac)
+;       (update-hmac-with-clear-payload)
+;       (deflate)
+;       (encrypt)
+;       (create-frame 1 1 
+
+(defn decode [token secret & {:keys [cipher salt] :or {cipher :aes-256}}]
+  "Decodes a OpenToken. Will throw an exception if the token is invalid."
   nil)
--- test/mch/opentoken_test.clj
+++ test/mch/opentoken_test.clj
@@ -2,6 +2,36 @@
   (:use clojure.test
         opentoken.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test {:otk "OTK"
+           :version 1
+           :cipher-suite 0
+           :iv nil
+           :key-info nil
+           :payload "foo=bar\r\nbar=baz"})
+
+(def expected {"foo" "bar" "bar" "baz"})
+
+(deftest aes-128
+  (testing "AES-128 decoding"
+    (let [cipher :aes-128
+          iv "a66C9MvM8eY4qJKyCXKW+w=="
+          token "UFRLAQK9THj0okLTUB663QrJFg5qA58IDhAb93ondvcx7sY6s44eszNqAAAga5W8Dc4XZwtsZ4qV3_lDI-Zn2_yadHHIhkGqNV5J9kw*"]
+      (is (= (decode token :cipher cipher :iv iv) expected)))))
+
+(deftest aes-256
+  (testing "AES-256 decoding"
+    (let [cipher :aes-256
+          iv "a66C9MvM8eY4qJKyCXKW+19PWDeuc3thDyuiumak+Dc="
+          token "UFRLAQEujlLGEvmVKDKyvL1vaZ27qMYhTxDSAZwtaufqUff7GQXTjvWBAAAgJJGPta7VOITap4uDZ_OkW_Kt4yYZ4BBQzw_NR2CNE-g*"]
+      (is (= (decode token :cipher cipher :iv iv) expected)))))
+
+(deftest 3des-168
+  (testing "3DES-168 decoding"
+    (let [cipher :3des-168
+          iv "a66C9MvM8eY4qJKyCXKW+19PWDeuc3th"
+          token "UFRLAQNoCsuAwybXOSBpIc9ZvxQVx_3fhghqSjy-pNJpfgAAGGlGgJ79NhX43lLRXAb9Mp5unR7XFWopzw**"]
+      (is (= (decode token :cipher cipher :iv iv) expected)))))
+
+(deftest multi-value
+  (testing "that keys with multiple values return a list of values")
+  (is (= 0 1)))
