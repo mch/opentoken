b863ec2914c34e88e93d8d5a0cde72cea30dbf8f
Matt Hughes <matt.c.hughes@gmail.com>
2013-04-06 10:21:26 -0600

    Moving the crypto stuff to it's own file.

--- src/mch/opentoken.clj
+++ src/mch/opentoken.clj
@@ -1,5 +1,6 @@
 (ns mch.opentoken
   "Implements the OpenToken specification: http://tools.ietf.org/html/draft-smith-opentoken-02"
+  (:use mch.opentoken.crypto)
   (:require [gloss core io])
   (:require [clojure.string :as str])
   (:require [clojure.data.codec.base64 :as b64]))
@@ -11,11 +12,8 @@
 ;;; - Move everything except the public API defs to mch.opentoken.encryption and so on.
 ;;; - Use streams to reduce copying, or some other method more suitable to functional composition. 
 
-(def cipher-suites {:none 0 :aes-256 1 :aes-128 2 :3des-168 3})
 (def opentoken-version 1)
 (def opentoken-literal "OTK")
-(def opentoken-default-salt (byte-array 8 (byte 0)))
-(def opentoken-default-iterations 1000)
 (def opentoken-standard-pairs {"subject" ""
                                "not-before" ""
                                "not-on-or-after" ""
@@ -86,91 +84,6 @@ since OpenToken allows for multiple values per key."
       (.write input 0 (count input))
       (.close))
     (.toByteArray out)))
-
-(defn encrypt-des [cleartext key iv]
-  (let [cipher (javax.crypto.Cipher/getInstance "DESede/CBC/PKCS5Padding")
-        _ (if (nil? iv)
-            (.init cipher javax.crypto.Cipher/ENCRYPT_MODE key)
-            (.init cipher javax.crypto.Cipher/ENCRYPT_MODE key (javax.crypto.spec.IvParameterSpec. iv)))
-        params (.getParameters cipher)
-        iv (.getIV cipher)
-        ciphertext (.doFinal cipher cleartext)]
-    {:iv iv :ciphertext ciphertext}))
-
-(defn encrypt-aes [cleartext key iv]
-  (let [cipher (javax.crypto.Cipher/getInstance "AES/CBC/PKCS5Padding")
-        _ (if (nil? iv)
-            (.init cipher javax.crypto.Cipher/ENCRYPT_MODE key)
-            (.init cipher javax.crypto.Cipher/ENCRYPT_MODE key (javax.crypto.spec.IvParameterSpec. iv)))
-        params (.getParameters cipher)
-        iv (.getIV cipher)
-        ciphertext (.doFinal cipher cleartext)]
-    {:iv iv :ciphertext ciphertext}))
-
-(defn decrypt-des [ciphertext key iv]
-  (let [cipher (javax.crypto.Cipher/getInstance "DESede/CBC/PKCS5Padding")
-        _ (.init cipher javax.crypto.Cipher/DECRYPT_MODE key (javax.crypto.spec.IvParameterSpec. iv))
-        cleartext (.doFinal cipher ciphertext)]
-    cleartext))
-
-(defn decrypt-aes [ciphertext key iv]
-  (let [cipher (javax.crypto.Cipher/getInstance "AES/CBC/PKCS5Padding")
-        _ (.init cipher javax.crypto.Cipher/DECRYPT_MODE key (javax.crypto.spec.IvParameterSpec. iv))
-        cleartext (.doFinal cipher ciphertext)]
-    cleartext))
-
-(defn make-aes-key [key-len password salt]  
-  (let [s (if (nil? salt) opentoken-default-salt
-              (if (string? salt) (.getBytes salt "UTF-8") salt))
-        pbe-name "PBKDF2WithHmacSHA1"
-        key-factory (javax.crypto.SecretKeyFactory/getInstance pbe-name)
-        key-spec (javax.crypto.spec.PBEKeySpec. (.toCharArray password) s opentoken-default-iterations key-len)
-        key (javax.crypto.spec.SecretKeySpec. (.getEncoded (.generateSecret key-factory key-spec)) "AES")]
-    key))
-
-(defn make-3des-key [key-len password salt]
-  (let [s (if (nil? salt) opentoken-default-salt
-              (if (string? salt) (.getBytes salt "UTF-8") salt))
-        pbe-name "PBEWithHmacSHA1AndDESede"
-        key-factory (javax.crypto.SecretKeyFactory/getInstance pbe-name)
-        key-spec (javax.crypto.spec.PBEKeySpec. (.toCharArray password) s opentoken-default-iterations key-len)
-        key (javax.crypto.spec.SecretKeySpec. (.getEncoded (.generateSecret key-factory key-spec)) "DESede")]
-    key))
-
-(defn make-key [cipher password salt]
-  (cond (= cipher :none) (byte-array 1 (byte 1))
-        (= cipher :aes-256) (make-aes-key 256 password salt)
-        (= cipher :aes-128) (make-aes-key 128 password salt)
-        (= cipher :3des-168) (make-3des-key 168 password salt)
-        :else (throw (IllegalArgumentException. "Invalid cipher."))))
-
-(defn make-key-from-ba [cipher key]
-  (cond (= cipher :aes-256) (javax.crypto.spec.SecretKeySpec. key "AES")
-        (= cipher :aes-128) (javax.crypto.spec.SecretKeySpec. key "AES")
-        (= cipher :3des-168) (javax.crypto.spec.SecretKeySpec. key "DESede")))
-
-(defn encrypt [cleartext & {:keys [cipher password salt key iv]
-                            :or {cipher :aes-256 password "" salt nil key nil iv nil}}]
-  (if-not (contains? cipher-suites cipher)
-    (throw (IllegalArgumentException. "Invalid cipher.")))
-  (let [k (if-not (nil? key)
-            (make-key-from-ba cipher key)
-            (make-key cipher password salt))
-        c (if (string? cleartext) (.getBytes cleartext "UTF-8") cleartext)]
-    (cond (= cipher :none) c
-          (contains? #{:aes-256 :aes-128} cipher) (encrypt-aes c k iv)
-          (= cipher :3des-168) (encrypt-des c k iv))))
-
-(defn decrypt [ciphertext & {:keys [cipher password salt key iv]
-                             :or {cipher :aes-256 password "" salt nil key nil iv nil}}]
-  (if-not (contains? cipher-suites cipher)
-    (throw (IllegalArgumentException. "Invalid cipher.")))
-  (let [k (if-not (nil? key)
-            (make-key-from-ba cipher key)
-            (make-key cipher password salt))]
-    (cond (= cipher :none) ciphertext
-          (contains? #{:aes-256 :aes-128} cipher) (decrypt-aes ciphertext k iv)
-          (= cipher :3des-168) (decrypt-des ciphertext k iv))))
 
 (defn create-frame [version cipher-suite hmac iv key-info payload]
   (.array (gloss.io/contiguous (gloss.io/encode opentoken
